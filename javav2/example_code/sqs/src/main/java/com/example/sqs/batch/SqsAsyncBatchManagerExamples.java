3835cc98135da6468f6e58e713d9e803e2553a56
Todd Hill <tkhill@amazon.com>
2024-10-15 10:04:52 -0400

    more sqs

--- javav2/example_code/sqs/src/main/java/com/example/sqs/batch/SqsAsyncBatchManagerExamples.java
+++ javav2/example_code/sqs/src/main/java/com/example/sqs/batch/SqsAsyncBatchManagerExamples.java
@@ -5,13 +5,20 @@ import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.batchmanager.SqsAsyncBatchManager;
 import software.amazon.awssdk.services.sqs.model.DeleteQueueResponse;
+import software.amazon.awssdk.services.sqs.model.QueueAttributeName;
 import software.amazon.awssdk.services.sqs.model.QueueDeletedRecentlyException;
 import software.amazon.awssdk.services.sqs.model.QueueDoesNotExistException;
 import software.amazon.awssdk.services.sqs.model.QueueNameExistsException;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
 
+import java.time.Duration;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.IntStream;
 
 public class SqsAsyncBatchManagerExamples {
@@ -20,50 +27,94 @@ public class SqsAsyncBatchManagerExamples {
     static String queueUrl = null;
     static final SqsAsyncClient sqsAsyncClient = SqsAsyncClient.create();
     static final SqsAsyncBatchManager sqsAsyncBatchManager = sqsAsyncClient.batchManager();
+    private static final Boolean CREATE_QUEUE = true;
+    private static final Boolean DELETE_QUEUE = true;
+    private static final Integer MESSAGE_COUNT = 100;
+    private static Integer RECEIVED_COUNT = 0;
 
     public static void main(String[] args) {
-        try {
-            queueUrl = createQueue().join();
-            logger.info("Queue {} created successfully with url {}", QUEUE_NAME, queueUrl);
-        } catch (CompletionException ce) {
-            Throwable cause = ce.getCause();
-            if (cause instanceof QueueDeletedRecentlyException) {
-                logger.error("Queue recently deleted: {}", cause.getMessage(), cause);
-            } else {
-                logger.error("Error creating queue: {}", cause.getMessage(), cause);
+        if (CREATE_QUEUE) {
+            try {
+                queueUrl = createQueue().join();
+                logger.info("Queue {} created successfully with url {}", QUEUE_NAME, queueUrl);
+            } catch (CompletionException ce) {
+                Throwable cause = ce.getCause();
+                if (cause instanceof QueueDeletedRecentlyException) {
+                    logger.error("Queue recently deleted: {}", cause.getMessage(), cause);
+                } else {
+                    logger.error("Error creating queue: {}", cause.getMessage(), cause);
+                }
+                return;
             }
-            return;
         }
-        sendMessages().join();
+        sendMessages(MESSAGE_COUNT).join();
         try {
-            deleteQueue().join();
-            logger.info("Queue {} deleted successfully", QUEUE_NAME);
-        } catch (CompletionException ce) {
-            Throwable cause = ce.getCause();
-            if (cause instanceof QueueDoesNotExistException) {
-                logger.error("Queue does not exist: {}", ce.getCause().getMessage(), cause);
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Integer loopCounter = 1;
+        while (RECEIVED_COUNT < MESSAGE_COUNT) {
+            logger.info("Loop countdown: {}", loopCounter);
+            receiveMessages();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            loopCounter++;
+        }
+
+        if (DELETE_QUEUE) {
+            try {
+                deleteQueue().join();
+                logger.info("Queue {} deleted successfully", QUEUE_NAME);
+            } catch (CompletionException ce) {
+                Throwable cause = ce.getCause();
+                if (cause instanceof QueueDoesNotExistException) {
+                    logger.error("Queue does not exist: {}", ce.getCause().getMessage(), cause);
+                }
             }
         }
     }
 
-    static CompletableFuture<Void> sendMessages() {
-        return CompletableFuture.runAsync(() ->
-                        IntStream.rangeClosed(1, 100).forEach(i ->
-                                sqsAsyncBatchManager.sendMessage(r -> r
-                                        .queueUrl(queueUrl)
-                                        .messageBody("Message " + i)))
+    static CompletableFuture<Void> sendMessages(Integer count) {
+            CompletableFuture[] futureMessageResponses = new CompletableFuture[count];
+            IntStream.range(0, count).forEach(i -> {
+                logger.info("sending single message {}", i);
+                futureMessageResponses[i] = sendSingleMessage("Message " + i);
+            });
+            return CompletableFuture.allOf(futureMessageResponses);
+    }
+
+    static CompletableFuture<SendMessageResponse> sendSingleMessage(String messageBody){
+        return sqsAsyncBatchManager.sendMessage(r -> r
+                .queueUrl(queueUrl)
+                .messageBody(messageBody));
+    }
+
+    static void receiveMessages(){
+        sqsAsyncBatchManager.receiveMessage(r -> r
+                        .queueUrl(queueUrl)
                 )
-                .whenComplete((resp, err) -> {
+                .whenComplete( (response, err) -> {
                     if (err != null) {
-                        logger.error("Error sending messages: {}", err.getCause().getMessage());
+                        logger.error("Error sending message: {}", err.getCause().getMessage());
+                    } else {
+                        RECEIVED_COUNT += response.messages().size();
+                        logger.info("Received {} messages", response.messages().size());
+                        logger.info("Total messages received: {}", RECEIVED_COUNT);
                     }
-                });
+                }).join();
     }
-
-
 
     static CompletableFuture<String> createQueue() {
-        return sqsAsyncClient.createQueue(r -> r.queueName(QUEUE_NAME))
+        return sqsAsyncClient.createQueue(r -> r
+                        .queueName(QUEUE_NAME)
+                        .attributes(Map.of(QueueAttributeName.VISIBILITY_TIMEOUT, Integer.toString(5 * 60)
+                         //       ,QueueAttributeName.RECEIVE_MESSAGE_WAIT_TIME_SECONDS, "20")
+        )))
                 .handle((resp, err) -> {
                     if (err != null) {
                         logger.error("Error creating queue: {}", err.getCause().getMessage());
