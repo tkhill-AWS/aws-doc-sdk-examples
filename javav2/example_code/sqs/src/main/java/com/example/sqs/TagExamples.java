e25dbb165367b980daee2c571f811d36426f07e5
Todd Hill <tkhill@amazon.com>
2025-02-25 14:56:18 -0500

    add tagging examples

---
+++ javav2/example_code/sqs/src/main/java/com/example/sqs/TagExamples.java
@@ -0,0 +1,69 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.sqs;
+// snippet-start:[sqs.java2.tag-examples]
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.ListQueueTagsResponse;
+import software.amazon.awssdk.services.sqs.model.SqsException;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class TagExamples {
+    static final SqsClient sqsClient = SqsClient.create();
+    static final String queueName = "TagExamples-queue-" + UUID.randomUUID().toString().replace("-", "").substring(0, 20);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TagExamples.class);
+
+    public static void main(String[] args) {
+        final String queueUrl = sqsClient.createQueue(b -> b.queueName(queueName)).queueUrl();
+        try {
+            addTags(queueUrl);
+            listTags(queueUrl);
+            removeTags(queueUrl);
+        } catch (SqsException e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            sqsClient.deleteQueue(b -> b.queueUrl(queueUrl));
+            sqsClient.close();
+        }
+    }
+
+    // snippet-start:[sqs.java2.add-tags-method]
+    public static void addTags(String queueUrl) {
+        // Build a map of the tags.
+        final Map<String, String> tagsToAdd = Map.of(
+                "Team", "Development",
+                "Priority", "Beta",
+                "Accounting ID", "456def");
+
+        // Add tags to the queue using a Consumer<TagQueueRequest.Builder> parameter.
+        sqsClient.tagQueue(b -> b
+                .queueUrl(queueUrl)
+                .tags(tagsToAdd)
+        );
+    }
+
+    public static void listTags(String queueUrl) {
+        // Call the listQueueTags method with a Consumer<ListQueueTagsRequest.Builder> parameter that creates a ListQueueTagsRequest.
+        ListQueueTagsResponse response = sqsClient.listQueueTags(b -> b
+                .queueUrl(queueUrl));
+
+        // Log the tags.
+        response.tags()
+                .forEach((k, v) ->
+                        LOGGER.info("Key: {} -> Value: {}", k, v));
+    }
+
+    public static void removeTags(String queueUrl) {
+        // Call the untagQueue method with a Consumer<UntagQueueRequest.Builder> parameter.
+        sqsClient.untagQueue(b -> b
+                .queueUrl(queueUrl)
+                .tagKeys("Accounting ID")
+        );
+    }
+}
+// snippet-end:[sqs.java2.tag-examples]
