c6b1a89574034e78ad38a14f6265edd3ec64e14c
Todd Hill <tkhill@amazon.com>
2025-03-04 17:28:08 -0500

    SQS with JMS examples

---
+++ javav2/example_code/sqs-jms/src/test/java/com/example/sqs/jms/stdqueue/SyncMessageReceiverClientAcknowledgeTest.java
@@ -0,0 +1,40 @@
+package com.example.sqs.jms.stdqueue;
+
+import com.example.sqs.jms.SqsJmsExampleUtils;
+import jakarta.jms.JMSException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+
+class SyncMessageReceiverClientAcknowledgeTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SyncMessageReceiverClientAcknowledgeTest.class);
+
+    @AfterEach
+    void tearDown() throws JMSException {
+        SqsJmsExampleUtils.cleanUpExample(SyncMessageReceiverClientAcknowledge.QUEUE_NAME, SqsJmsExampleUtils.QUEUE_VISIBILITY_TIMEOUT );
+    }
+
+    @Test
+    @Tag("IntegrationTest")
+    void doReceiveMessagesSyncClientAcknowledgeTest() {
+        try {
+            SyncMessageReceiverClientAcknowledge.doReceiveMessagesSyncClientAcknowledge();
+        } catch (JMSException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+
+        // Use plain SQS test to make sure there are no messages in the queue.
+        try (SqsClient sqsClient = SqsClient.create()) {
+            ReceiveMessageResponse response = sqsClient.receiveMessage(b -> b.queueUrl(sqsClient
+                    .getQueueUrl(builder -> builder
+                            .queueName(SyncMessageReceiverClientAcknowledge.QUEUE_NAME)).queueUrl()));
+
+            Assertions.assertFalse(response.hasMessages());
+        }
+    }
+}
---
+++ javav2/example_code/sqs-jms/src/test/java/com/example/sqs/jms/stdqueue/TextMessageSenderTest.java
@@ -0,0 +1,14 @@
+package com.example.sqs.jms.stdqueue;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class TextMessageSenderTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TextMessageSenderTest.class);
+    @Test
+    void doSendTextMessageTest() {
+        // The TextMessageSender.doSendTextMessage() can't be tested with an automated test because it requires user input.
+    }
+
+}
